ca2308b3e33a625d881dbdf5dea0b9e6fe80774b
MOON <answnsgh41@gmail.com>
2023-01-05 14:56:53 +0900

    3일차

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//자동슬라이드
 $(document).ready(function(){
   var slideI = 0;
   setInterval(function(){
@@ -11,9 +12,28 @@ $(document).ready(function(){
     });
   }, 3000);
 });
-
+// 검색창
 $("#quest").click(function(){
   $(".question").slideToggle(50);
 });
-
-
+// 메뉴
+$('.nav1').mouseover(function(){
+  $('.submenu').stop().slideDown();
+});
+$('.nav2').mouseover(function(){
+  $('.submenu').stop().slideDown();
+});
+$('.question').mouseover(function(){
+  $('.submenu').stop().slideDown();
+});
+$('.submenu').mouseover(function(){
+  $('.this').stop().slideDown();
+});
+$('.submenu').mouseout(function(){
+  $('.submenu').stop().slideUp();
+});
+//카드
+$('.pic1').mouseover(function () {
+  $('.large').eq(0).fadeIn(500);
+  // $('.large').eq(0).Attr('display', 'block');
+});
